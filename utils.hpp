1be3713d6af0f0837ca30b68f9a5c65a7e72dfdb
ad71 <ads6may@gmail.com>
2018-09-01 21:11:39 +0530

    Added removeall for std::vector<T>

--- utils.hpp
+++ utils.hpp
@@ -4,12 +4,25 @@
 #include <vector>
 #include <ostream>
 
+// Display a vector of type T to the required output stream
 template<typename T>
-void print_vector(std::ostream& out, std::vector<T> seq) {
+void print_vector(std::ostream& out, const std::vector<T>& seq) {
     for (std::size_t i = 0; i < seq.size(); ++i) {
         out << seq[i] << " ";
     }
     out << '\n';
+}
+
+// Return a copy of a vector with all occurences of `item` removed
+template<typename T>
+std::vector<T> removeall(const T item, const std::vector<T>& seq) {
+    std::vector<T> n_v;
+    for (std::size_t i = 0; i < seq.size(); ++i) {
+        if (seq[i] != item) {
+            n_v.push_back(seq[i]);
+        }
+    }
+    return n_v;
 }
 
 #endif
