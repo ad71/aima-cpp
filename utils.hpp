51a98b40c32e6b58e36b2146386324e668f3b8cf
ad71 <ads6may@gmail.com>
2018-09-01 22:00:12 +0530

    replace iterator with auto

--- utils.hpp
+++ utils.hpp
@@ -2,13 +2,14 @@
 #define UTILS_HPP_
 
 #include <vector>
+#include <string>
 #include <ostream>
 
 // Display a vector of type T to the required output stream
 template<typename T>
 void print_vector(std::ostream& out, const std::vector<T>& seq) {
-    for (std::size_t i = 0; i < seq.size(); ++i) {
-        out << seq[i] << " ";
+    for (auto i : seq) {
+        out << i << " ";
     }
     out << '\n';
 }
@@ -17,9 +18,9 @@ void print_vector(std::ostream& out, const std::vector<T>& seq) {
 template<typename T>
 std::vector<T> removeall(const T item, const std::vector<T>& seq) {
     std::vector<T> n_v;
-    for (std::size_t i = 0; i < seq.size(); ++i) {
-        if (seq[i] != item) {
-            n_v.push_back(seq[i]);
+    for (auto i : seq) {
+        if (i != item) {
+            n_v.push_back(i);
         }
     }
     return n_v;
